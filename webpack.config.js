7940413023fcffdd5ebb9e2c66602d2c3a2b98fd
Даниил Высоцкий <Vysotskiy.D@terempro.loc>
2017-07-31 11:24:20 +0300

    ready to stage

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
 var path = require('path')
 var webpack = require('webpack')
+var CompressionPlugin = require("compression-webpack-plugin")
 
 module.exports = {
-  entry: './src/app.js',
+  entry: [    
+    path.join(__dirname, '/src/app.js')
+  ],
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this necessary.
-            'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
           // other vue-loader options go here
         }
       },
-      {
+      { 
         test: /\.js$/,
-        loader: 'babel-loader',
+        loader: 'babel-loader',        
         exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
@@ -42,33 +45,42 @@ module.exports = {
       'vue$': 'vue/dist/vue.esm.js'
     }
   },
-  devServer: {
-    historyApiFallback: true,
-    noInfo: true
-  },
   performance: {
     hints: false
   },
-  devtool: '#eval-source-map'
+  devtool: '#eval-source-map'  
 }
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
-}
+module.exports.devtool = '#source-map'
+module.exports.plugins = (module.exports.plugins || []).concat([
+  new webpack.optimize.OccurrenceOrderPlugin(),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"production"'
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true,
+    beautify: false,
+    comments: false,
+    compress: {
+      sequences     : true,
+      booleans      : true,
+      loops         : true,
+      unused      : true,
+      warnings    : false,
+      drop_console: true,
+      unsafe      : true
+    }
+  }),
+  new CompressionPlugin({
+    asset: "[path].gz[query]",
+    algorithm: "gzip",
+    test: /\.(js|css|html|svg)$/,
+    threshold: 10240,
+    minRatio: 0.8
+  }),
+  new webpack.LoaderOptionsPlugin({
+    minimize: true
+  })
+])
