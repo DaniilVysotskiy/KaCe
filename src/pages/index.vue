4f04c70a68e18cecb444a2af163f46cf127af0ec
Даниил Высоцкий <Vysotskiy.D@terempro.loc>
2017-07-31 12:32:38 +0300

    step 2
    Signed-off-by: Даниил Высоцкий <Vysotskiy.D@terempro.loc>

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="index">
+		<img src="../assets/img/logo_black.png" alt="Logo">
+		<div class="btn">
+			<router-link tag="a" to="/vuejs">ВХОД НА САЙТ</router-link>
+		</div>
+	</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" scoped>
+	@import '../assets/style/index'
+</style>
